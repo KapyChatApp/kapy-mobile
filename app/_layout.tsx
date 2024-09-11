aee171a00fdcd7a98206142177a77a9c59af5928
HoangLam21 <hoanglamdeveloper@gmail.com>
2024-09-11 11:14:23 +0700

    setup clerk auth

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,15 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
-
+import { ClerkLoaded, ClerkProvider } from '@clerk/clerk-expo'
+import { Slot } from 'expo-router'
+const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!
 export default function _layout() {
   return (
-    <View>
-      <Text>Hello Kapy</Text>
-    </View>
+    <ClerkProvider publishableKey={publishableKey}>
+    <ClerkLoaded>
+      <Slot />
+    </ClerkLoaded>
+  </ClerkProvider>
   )
 }
 
