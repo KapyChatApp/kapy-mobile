d0f733580cdf607952f32c96cf5af79da287b540
HoangLam21 <hoanglamdeveloper@gmail.com>
2024-09-18 17:25:22 +0700

    fix friends-filter size

--- components/ui/FriendFilter.tsx
+++ components/ui/FriendFilter.tsx
@@ -23,26 +23,26 @@ const FriendFilter = ({
     <View className="relative z-20">
       <TouchableOpacity
         onPress={() => setIsOpen(!isOpen)}
-        className="flex right-[22px] -top-[8px] items-center justify-center border border-border w-[90px] h-[22px] rounded-full z-20"
+        className="flex right-[22px] -top-[8px] items-center justify-center border border-border w-[90px] h-[30px] rounded-full z-20"
       >
         <Text className="text-14  font-helvetica-bold text-deny text-center">
           View by
         </Text>
       </TouchableOpacity>
-      {isOpen? (<View className="absolute w-screen h-screen"></View>):null}
+      
       {isOpen ? (
-        <View className="absolute bg-white z-10 right-[22px] top-[30px]">
+        <View className="absolute bg-white z-10 right-[22px] top-[20px] w-[160px]">
           <TouchableOpacity
             className="flex items-center justify-center p-[12px] border border-border"
             onPress={filter1}
           >
-            <Text className="text-12 font-helvetica-light">Only Friends</Text>
+            <Text className="text-14 font-helvetica-light">Only Friends</Text>
           </TouchableOpacity>
           <TouchableOpacity
             className="flex items-center justify-center p-[12px] border border-border"
             onPress={filter2}
           >
-            <Text className="text-12 font-helvetica-light">
+            <Text className="text-14 font-helvetica-light">
               Only Bestfriends
             </Text>
           </TouchableOpacity>
@@ -50,13 +50,13 @@ const FriendFilter = ({
             className="flex items-center justify-center p-[12px] border border-border"
             onPress={filter3}
           >
-            <Text className="text-12 font-helvetica-light">Alphabet</Text>
+            <Text className="text-14 font-helvetica-light">Alphabet</Text>
           </TouchableOpacity>
           <TouchableOpacity
             className="flex items-center justify-center p-[12px] border border-border"
             onPress={filter4}
           >
-            <Text className="text-12 font-helvetica-light">
+            <Text className="text-14 font-helvetica-light">
               Time increasing
             </Text>
           </TouchableOpacity>
@@ -64,7 +64,7 @@ const FriendFilter = ({
             className="flex items-center justify-center p-[12px] border border-border"
             onPress={filter5}
           >
-            <Text className="text-12 font-helvetica-light">
+            <Text className="text-14 font-helvetica-light">
               Time decreasing
             </Text>
           </TouchableOpacity>
