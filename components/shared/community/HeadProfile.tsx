2ff8704681a73bcdb7db0b05047d15dc06514f4d
HoangLam21 <hoanglamdeveloper@gmail.com>
2024-10-10 16:51:11 +0700

    my-wall page

--- components/shared/community/HeadProfile.tsx
+++ components/shared/community/HeadProfile.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import Previous from "@/components/ui/Previous";
 import { useNavigation } from "expo-router";
 import UserAvatar from "@/components/ui/UserAvatar";
+import { textLight0Dark500 } from "@/styles/theme";
 
 const HeadProfile = () => {
   return (
@@ -11,11 +12,11 @@ const HeadProfile = () => {
       <View className=" flex items-center justify-center absolute top-[90px]">
         <UserAvatar size={176}></UserAvatar>
         <Text className="text-24 font-helvetica-bold text-cardinal mt-[4px]">Name</Text>
-        <Text className="text-18 font-helvetica-light my-[2px]">(Nickname)</Text>
-        <Text className="text-16 font-helvetica-light my-[2px]">Description</Text>
+        <Text className={`text-18 font-helvetica-light my-[2px] ${textLight0Dark500}`}>(Nickname)</Text>
+        <Text className={`text-16 font-helvetica-light my-[2px] ${textLight0Dark500}`}>Description</Text>
         <View className="flex flex-row items-center justify-center">
-          <Text className="text-14 font-helvetica-light">Community Point: </Text>
-          <Text className="text-18 font-helvetica-bold color-cardinal">100</Text>
+          <Text className={`text-14 font-helvetica-light ${textLight0Dark500}`}>Community Point: </Text>
+          <Text className="text-18 font-helvetica-bold text-cardinal">100</Text>
         </View>
       </View>
     </View>
