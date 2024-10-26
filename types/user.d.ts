f6315547f464bf9618b1c96289c57411d2bb39d7
HoangLam21 <hoanglamdeveloper@gmail.com>
2024-10-26 13:16:17 +0700

    add nickname icon

---
+++ types/user.d.ts
@@ -0,0 +1,11 @@
+export interface UserRegisterProps{
+  firstName: string;
+  lastName: string;
+  nickName: string;
+  phoneNumber: string;
+  email: string;
+  password: string;
+  rePassword:string;
+  gender: boolean;
+  birthDay: Date;
+}
