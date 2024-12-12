a884a0e6a2bd32c694f86853ec4ef3baa299546a
HoangLam21 <hoanglamdeveloper@gmail.com>
2024-12-12 16:27:28 +0700

    report content list

--- app/report/index.tsx
+++ app/report/index.tsx
@@ -1,12 +1,15 @@
 import { View, Text, TouchableOpacity } from "react-native";
-import React from "react";
+import React, { useState } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { useNavigation } from "expo-router";
 import Previous from "@/components/ui/Previous";
 import { bgLight500Dark10, textLight0Dark500 } from "@/styles/theme";
+import { ReportContents } from "@/constants/UiItems";
 
 const ReportPage = () => {
   const navigation = useNavigation();
+  const [selectedContents, setSelectedContents] = useState<string[]>([]);
+
   return (
     <SafeAreaView className={`${bgLight500Dark10} flex-1`}>
       <View className="mt-[10px] ml-[10px]">
@@ -17,49 +20,25 @@ const ReportPage = () => {
       >
         Why do you report it?
       </Text>
-      <View className="px-[12px]">
-        <TouchableOpacity className="flex px-[10px] border-y-[0.5px] border-border  py-[14px]">
+      {ReportContents.map((item, index) => (
+        <TouchableOpacity
+          onPress={() => { if(!selectedContents.includes(item)){setSelectedContents((prev) => [...prev, item])}else{
+            setSelectedContents((prev)=>prev.filter((i)=>i!==item));
+          }}}
+          key={index}
+          className={`flex px-[10px] border-t-[0.5px] border-border  py-[14px] ${
+            index === ReportContents.length - 1 ? "border-y-[0.5px]" : ""
+          } ${
+            selectedContents.includes(item)
+              ? "bg-light-340 dark:bg-dark-20"
+              : ""
+          }`}
+        >
           <Text className={`${textLight0Dark500} font-helvetica-light text-14`}>
-            Copyright infringement or plagiarism
+            {item}
           </Text>
         </TouchableOpacity>
-        <TouchableOpacity className="flex px-[10px] border-y-[0.5px] border-border  py-[14px]">
-          <Text className={`${textLight0Dark500} font-helvetica-light text-14`}>
-            Harassment or bullying
-          </Text>
-        </TouchableOpacity>
-        <TouchableOpacity className="flex px-[10px] border-y-[0.5px] border-border  py-[14px]">
-          <Text className={`${textLight0Dark500} font-helvetica-light text-14`}>
-            Posting sensitive or private information
-          </Text>
-        </TouchableOpacity>
-        <TouchableOpacity className="flex px-[10px] border-y-[0.5px] border-border  py-[14px]">
-          <Text className={`${textLight0Dark500} font-helvetica-light text-14`}>
-            Spamming or scam activities
-          </Text>
-        </TouchableOpacity>
-        <TouchableOpacity className="flex px-[10px] border-y-[0.5px] border-border  py-[14px]">
-          <Text className={`${textLight0Dark500} font-helvetica-light text-14`}>
-          Nudity and Sexual Content
-          </Text>
-        </TouchableOpacity>
-        <TouchableOpacity className="flex px-[10px] border-y-[0.5px] border-border  py-[14px]">
-          <Text className={`${textLight0Dark500} font-helvetica-light text-14`}>
-          Terrorism and Extremism
-          </Text>
-        </TouchableOpacity>
-        <TouchableOpacity className="flex px-[10px] border-y-[0.5px] border-border  py-[14px]">
-          <Text className={`${textLight0Dark500} font-helvetica-light text-14`}>
-          Hate Speech
-          </Text>
-        </TouchableOpacity>
-        <TouchableOpacity className="flex px-[10px] border-y-[0.5px] border-border  py-[14px]">
-          <Text className={`${textLight0Dark500} font-helvetica-light text-14`}>
-          Child Exploitation
-          </Text>
-        </TouchableOpacity>
-     
-      </View>
+      ))}
     </SafeAreaView>
   );
 };
