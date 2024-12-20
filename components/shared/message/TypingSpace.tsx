26383456c45a295e4b5b039689ac525ab1e261e3
HoangLam21 <hoanglamdeveloper@gmail.com>
2024-12-20 18:18:34 +0700

    Typingmessage Animation

--- components/shared/message/TypingSpace.tsx
+++ components/shared/message/TypingSpace.tsx
@@ -1,15 +1,11 @@
-import { View} from "react-native";
 import React, { useRef, useState } from "react";
-import {
-  bgLight600Dark300,
-  textLight0Dark500,
-} from "@/styles/theme";
-import { TextInput } from "react-native";
-import { TouchableOpacity } from "react-native-gesture-handler";
-import { IconURL } from "@/constants/IconURL";
-import Icon from "@/components/ui/Icon";
+import { View, TextInput, TouchableOpacity } from "react-native";
 import { useTheme } from "@/context/ThemeProviders";
-import { useClickOutside } from "react-native-click-outside";
+import Icon from "@/components/ui/Icon";
+import { IconURL } from "@/constants/IconURL";
+import { bgLight600Dark300, textLight0Dark500 } from "@/styles/theme";
+import Animated, { Easing } from "react-native-reanimated";
+import { useSharedValue, withTiming, useAnimatedStyle } from "react-native-reanimated";
 
 const TypingSpace = ({
   isTyping,
@@ -33,66 +29,66 @@ const TypingSpace = ({
   setIsMicroOpen: any;
 }) => {
   const { theme } = useTheme();
-  const [isMoreOpen, setIsMoreOpen] = useState(false);
-  const ref = useClickOutside<View>(() => setIsMoreOpen(false));
-  const buttonRef = useRef<View | null>(null);
+  const animatedWidth = useSharedValue(1);
 
-  const [popoverPosition, setPopoverPosition] = useState({
-    bottom: 0,
-    left: 0,
+  React.useEffect(() => {
+    animatedWidth.value = withTiming(isTyping ? 0 : 1, {
+      duration: 150,
+      easing: Easing.inOut(Easing.ease),
+    });
+  }, [isTyping]);
+
+  const animatedStyle = useAnimatedStyle(() => {
+    return {
+      width: animatedWidth.value * 180, 
+    };
   });
 
-  const handleToggle = () => {
-    buttonRef.current?.measure((fx, fy, width, height, px, py) => {
-      setPopoverPosition({ bottom: fy + height, left: 0 });
-      setIsMoreOpen(true);
-    });
-  };
   return (
     <View
       className={`w-full h-[66] bg-light-500 dark:bg-dark-0 flex flex-row justify-between items-center px-[10px]`}
       style={{ columnGap: 8 }}
     >
-      {isTyping ? null : (
-        <View
-          className="flex flex-row items-center justify-between"
-          style={{ columnGap: 11 }}
-        >
-           <TouchableOpacity onPress={handlePickDocument}>
-            <Icon
-              iconURL={theme === "light" ? IconURL.attach_l : IconURL.attach_d}
-              size={28}
-            ></Icon>
-          </TouchableOpacity>
-          <TouchableOpacity>
-            <Icon
-              iconURL={theme === "light" ? IconURL.location_l : IconURL.location_d}
-              size={28}
-            ></Icon>
-          </TouchableOpacity>
-          <TouchableOpacity onPress={setIsMicroOpen}>
-            <Icon
-              iconURL={theme === "light" ? IconURL.mic_l : IconURL.mic_d}
-              size={28}
-            ></Icon>
-          </TouchableOpacity>
-          <TouchableOpacity onPress={handlePickMedia}>
-            <Icon
-              iconURL={theme === "light" ? IconURL.image_l : IconURL.image_d}
-              size={28}
-            ></Icon>
-          </TouchableOpacity>
-          <TouchableOpacity onPress={setIsCameraOpen}>
-            <Icon
-              iconURL={
-                theme === "light" ? IconURL.opencam_l : IconURL.opencam_d
-              }
-              size={28}
-            ></Icon>
-          </TouchableOpacity>
-        </View>
-      )}
-      {isTyping ? (
+      <Animated.View
+        style={[{ flexDirection: "row", justifyContent: "space-between", columnGap: 7 }, animatedStyle]} 
+      >
+        {!isTyping && (
+          <>
+            <TouchableOpacity onPress={handlePickDocument}>
+              <Icon
+                iconURL={theme === "light" ? IconURL.attach_l : IconURL.attach_d}
+                size={28}
+              />
+            </TouchableOpacity>
+            <TouchableOpacity>
+              <Icon
+                iconURL={theme === "light" ? IconURL.location_l : IconURL.location_d}
+                size={28}
+              />
+            </TouchableOpacity>
+            <TouchableOpacity onPress={setIsMicroOpen}>
+              <Icon
+                iconURL={theme === "light" ? IconURL.mic_l : IconURL.mic_d}
+                size={28}
+              />
+            </TouchableOpacity>
+            <TouchableOpacity onPress={handlePickMedia}>
+              <Icon
+                iconURL={theme === "light" ? IconURL.image_l : IconURL.image_d}
+                size={28}
+              />
+            </TouchableOpacity>
+            <TouchableOpacity onPress={setIsCameraOpen}>
+              <Icon
+                iconURL={theme === "light" ? IconURL.opencam_l : IconURL.opencam_d}
+                size={28}
+              />
+            </TouchableOpacity>
+          </>
+        )}
+      </Animated.View>
+
+      {isTyping && (
         <TouchableOpacity
           onPress={() => {
             setIsTypeping(false);
@@ -100,16 +96,17 @@ const TypingSpace = ({
         >
           <Icon iconURL={IconURL.plus} size={28} />
         </TouchableOpacity>
-      ) : null}
-      <View className="flex-1">
+      )}
+
+      <View style={{ flex: 1 }}>
         <TextInput
           placeholder="Type..."
           placeholderTextColor="#A9A9A9"
-          className={`h-[42px] w-full rounded-full ${bgLight600Dark300} px-[12px] ${textLight0Dark500} font-helvetica-light`}
+          className={`h-[42px] rounded-full ${bgLight600Dark300} px-[12px] ${textLight0Dark500} font-helvetica-light`}
           onFocus={() => setIsTypeping(true)}
           onChangeText={onChangeText}
           value={value}
-        ></TextInput>
+        />
       </View>
 
       <View>
